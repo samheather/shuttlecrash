8d0657428cdde00a4bf1783ae4bd9619ea9cfe93
Zisu Andrei <matzipan@gmail.com>
2013-04-20 15:56:12 +0100

    Getting everything running

--- public/js/game.js
+++ public/js/game.js
@@ -1,165 +1,11 @@
 /**
 	@class Example code in the form of a simple "falling game" where the player must try to stay on the screen as platforms move past them upward.
 */
-function ExampleGame(gs) {
-	// seedable random number generator for building the world
-	var r = new SeedableRandom();
+function ShuttleCrash(gs) {
 	var d = new Date;
-	r.seed(d.getTime());
 	// vertical separation of platform layers
 	X_SEPARATION = 300;
-	Y_SEPARATION = 150;
-		
-		/* Concurrency stuff */
-		
-		// draw the player's sprite every frame
-		this.draw = function(c) {
-			p.draw(c, world.camera(pos));
-		}
-		
-		this.updateanimation = function() {
-			if (vy > world.gravity * 2) {
-				p.action("fall");
-			} else {
-				if (vx >= WALK_VX) {
-					p.action("walk right");
-				} else if (vx <= -WALK_VX) {
-					p.action("walk left");
-				} else {
-					p.action("stand");
-				}
-			}
-		}
-		
-		// update the player's position every frame
-		this.update = function() {
-			vy = Math.min(vy + world.gravity, MAX_VY);
-			this.updateanimation();
-			pos[0] += vx;
-			pos[1] += vy;
-			p.update();
-			if (pos[1] > p.height + gs.height || pos[1] < 0) {
-				document.getElementById("gameover").style.paddingTop = gs.height / 2 - 100;
-				document.getElementById("gameover").style.display = "block";
-			}
-		}
-		
-		/* collision stuff */
-		
-		// return the bounding box of our sprite for the collision test
-		this.get_collision_aabb = function() {
-			return p.aabb(pos);
-		}
-		
-		/* input events stuff */
-		this.keyDown_37 = function () {
-			this.updateanimation();
-			vx = -WALK_VX;
-		}
-		
-		this.keyUp_37 = this.keyUp_39 = function() {
-			this.updateanimation();
-			vx = 0;
-		}
-		
-		this.keyDown_39 = function () {
-			this.updateanimation();
-			vx = WALK_VX;
-		}
-		
-		// basic comparison function
-		var cmp = function(x, y){ return x[0] < y[0] ? 1 : x[0] > y[0] ? -1 : 0; };
-		
-		// if the axis aligned bouding box of this entity collides with another
-		this.collide_aabb = function(who) {
-			if (who.type == 'platform') {
-				var ab = this.get_collision_aabb();
-				var bb = who.get_collision_aabb();
-				
-				var sides = [
-						[bb[1] - (ab[1] + ab[3]), 1, 1],
-						[bb[0] - (ab[0] + ab[2]), 0, 1],
-						[ab[0] - (bb[0] + bb[2]), 0, -1],
-						[ab[1] - (bb[1] + bb[3]), 1, -1]
-				];
-				sides.sort(cmp);
-				var d = sides[0];
-				// hit a vertical side
-				if (d[1]) {
-					if (pos[0] > bb[0] + bb[2]) {
-						pos[0] += WALK_VX;
-					} else if (pos[0] < bb[0]) {
-						pos[0] -= WALK_VX;
-					} else {
-						pos[1] = bb[1];
-						vy = 0;
-						this.updateanimation();
-					}
-				} else {
-					// horizontal side
-					if (pos[0] > bb[0] + bb[2]) {
-						pos[0] += WALK_VX;
-					} else if (pos[0] < bb[0]) {
-						pos[0] -= WALK_VX;
-					}
-				}
-			}
-		}
-		/*this.keyDown = function (keyCode) {
-			console.log(keyCode);
-		}*/
-	}
-	
-	/* Platform */
-	function Platform(world, pos) {
-		this.type = 'platform';
-		// the list of props sitting on this platform
-		var props = [];
-		// current position
-		this.pos = pos;
-		// closureify
-		var platform = this;
-		
-		// get the sprite for this platform
-		var p = this.sprite = new Sprite(["center", "top"], {default: [["img/platform-" + r.nextInt(1, 5) + ".png", 0]]}, function() {
-			// once the sprite is loaded do some init
-			p.action("default");
-		});
-		
-		// called when this entity is added
-		this.init = function() {
-			// make me some props
-			for (var i = 0; i < r.nextInt(1, 4); i++) {
-				props.push(new Prop(world, this));
-				gs.addEntity(props[props.length - 1]);
-			}
-		}
-		
-		// update this platform's position every frame
-		this.update = function() {
-			pos[1] -= world.upspeed;
-			// if the platform has moved off the screen the get rid of it
-			if (pos[1] < -200) {
-				for (var i = 0; i < props.length; i++) {
-					gs.delEntity(props[i]);
-					delete props[i];
-				}
-				gs.delEntity(this);
-				world.remove(this);
-			}
-			p.update();
-		}
-		
-		// draw this platform's sprite every frame
-		this.draw = function(c) {
-			p.draw(c, world.camera(pos));
-		}
-		
-		// return the bounding box of our sprite for the aabb collision test
-		this.get_collision_aabb = function() {
-			return p.aabb(pos);
-		}
-	}
+	Y_SEPARATION = 150;	
 	
 	/* World */
 	function World() {
@@ -176,7 +22,7 @@ function ExampleGame(gs) {
 		
 		// background colour
 		var bg = 'rgba(240, 255, 255, 1.0)';
-		var player = new Player(this);
+		var player = new Shuttle(this, gs);
 		var platforms = [];
 		
 		// defines a simple screen-relative camera method
@@ -187,7 +33,7 @@ function ExampleGame(gs) {
 		// called when we are first added
 		this.init = function() {
 			gs.addEntity(player);
-			platforms.push(gs.addEntity(new Platform(this, [gs.width / 2, gs.height / 2])));
+			//platforms.push(gs.addEntity(new Platform(this, [gs.width / 2, gs.height / 2])));
 			for (var y = gs.height / 2 + Y_SEPARATION; y < gs.height + Y_SEPARATION; y += Y_SEPARATION) {
 				this.addrow(y);
 			}
@@ -201,7 +47,7 @@ function ExampleGame(gs) {
 		// called every frame to run the game, collisions, etc.
 		this.update = function() {
 			// detect collisions between the player and the props
-			collide.aabb([player], platforms);
+			//collide.aabb([player], platforms);
 			// update the camera position
 			this.cpos += (player.pos[0] - this.cpos) * 0.5;
 			// increment the total distance travelled
@@ -242,9 +88,9 @@ function ExampleGame(gs) {
 		
 		// add a new random row of platforms to the world
 		this.addrow = function(y) {
-			for (var x = player.pos[0] - gs.width / 2; x < player.pos[0] + gs.width / 2; x += X_SEPARATION) {
-				platforms.push(gs.addEntity(new Platform(this, [x, y])));
-			}
+			//for (var x = player.pos[0] - gs.width / 2; x < player.pos[0] + gs.width / 2; x += X_SEPARATION) {
+			//	platforms.push(gs.addEntity(new Platform(this, [x, y])));
+			//}
 		}
 	}
 	
@@ -259,7 +105,7 @@ function ExampleGame(gs) {
 
 // this launch function is called by the HTML document to start the game
 // the HTML document has a div tag called 'surface'
-function launch() {
+JSGameSoup.ready(function () {
 	// grab the surface div and insert a canvas of the same size inside it
 	var surface = document.getElementById("surface");
 	var newcanvas = document.createElement("canvas");
@@ -269,6 +115,6 @@ function launch() {
 	surface.appendChild(newcanvas);
 	// launch the gamesoup loop on the new canvas we just created
 	var gs = new JSGameSoup(newcanvas, 30);
-	ExampleGame(gs);
+	ShuttleCrash(gs);
 	gs.launch();
-}
+});
