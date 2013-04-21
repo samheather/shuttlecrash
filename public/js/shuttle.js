c01549e141213ae2a09bfef070a314299f4f5be4
Zisu Andrei <matzipan@gmail.com>
2013-04-21 11:32:27 +0100

    Make the shuttle launch from the space station position

--- public/js/shuttle.js
+++ public/js/shuttle.js
@@ -39,7 +39,7 @@ function Shuttle(gs) {
 	
 	this.init = function () {
 		sprites.action("stopped");
-		pos= [sprites.get_size()[1]/2+100, gs.height / 2];
+		pos= [sprites.get_size()[0]/2+20, sprites.get_size()[1]];
 		sprites.angle(attackAngle*Math.PI/180);
 		//this.set_state("stopped"); @TODO after menu impelementation
 		this.set_state("falling");
